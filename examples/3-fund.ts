39b09053aa5f6df7fa6d53a3b4cd757fad831a29
avernikoz <avernikoz@gmail.com>
2024-04-03 15:38:12 +0300

    simplify example for fund & boosted fund

--- examples/3-fund.ts
+++ examples/3-fund.ts
@@ -9,27 +9,25 @@ import {
 
 // yarn ts-node examples/fund.ts
 (async () => {
-  const CoinObjectId = process.argv[2]!;
-
   const contractAddress = validateEnvVariable("REFUND_PACKAGE_ADDRESS");
   const poolId = validateEnvVariable("REFUND_POOL_OBJECT_ID");
   const keypairBech32 = validateEnvVariable("KEYPAIR_BECH32");
+  const keypair = Ed25519Keypair.fromSecretKey(
+    hexStringToUint8Array(bech32ToHex(keypairBech32))
+  );
 
   const txb = new TransactionBlock();
+  const [coin] = txb.splitCoins(txb.gas, [txb.pure(197600741882442, "u64")]);
 
   txb.moveCall({
     target: `${contractAddress}::refund::fund`,
-    arguments: [txb.object(poolId), txb.object(CoinObjectId)],
+    arguments: [txb.object(poolId), coin],
   });
 
   // const res = await provider.devInspectTransactionBlock({
   //   sender: user,
   //   transactionBlock: txData.tx,
   // });
-
-  const keypair = Ed25519Keypair.fromSecretKey(
-    hexStringToUint8Array(bech32ToHex(keypairBech32))
-  );
 
   const res = await signAndExecuteTransaction(txb, keypair);
 
--- examples/4-fund-boosted.ts
+++ examples/4-fund-boosted.ts
@@ -9,27 +9,25 @@ import {
 
 // yarn ts-node examples/4-fund-boosted.ts
 (async () => {
-  const CoinObjectId = process.argv[2]!;
-
   const contractAddress = validateEnvVariable("REFUND_PACKAGE_ADDRESS");
   const poolId = validateEnvVariable("REFUND_POOL_OBJECT_ID");
   const keypairBech32 = validateEnvVariable("KEYPAIR_BECH32");
+  const keypair = Ed25519Keypair.fromSecretKey(
+    hexStringToUint8Array(bech32ToHex(keypairBech32))
+  );
 
   const txb = new TransactionBlock();
+  const [coin] = txb.splitCoins(txb.gas, [txb.pure(98800370941221, "u64")]);
 
   txb.moveCall({
     target: `${contractAddress}::booster::fund`,
-    arguments: [txb.object(poolId), txb.object(CoinObjectId)],
+    arguments: [txb.object(poolId), coin],
   });
 
   // const res = await provider.devInspectTransactionBlock({
   //   sender: user,
   //   transactionBlock: txData.tx,
   // });
-
-  const keypair = Ed25519Keypair.fromSecretKey(
-    hexStringToUint8Array(bech32ToHex(keypairBech32))
-  );
 
   const res = await signAndExecuteTransaction(txb, keypair);
 
